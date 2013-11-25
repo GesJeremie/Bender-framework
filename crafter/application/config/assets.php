d4a8ca4910f4364fcf54580457be968feda8b143
jeremie_ges <bonjour@gesjeremie.fr>
2013-11-25 03:02:47 +0100

    Init crafter folder

---
+++ crafter/application/config/assets.php
@@ -0,0 +1,82 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| Environment compress
+|--------------------------------------------------------------------------
+|
+| Here you can choose if you want use the compress system in :
+| development / testing / production mode
+| 
+*/
+$assets['compress'] = array(
+
+	'development' => FALSE,
+	'testing'     => FALSE,
+	'production'  => FALSE
+ 
+	);
+
+/*
+|--------------------------------------------------------------------------
+| Register css
+|--------------------------------------------------------------------------
+|
+| Here you can register all css used by your application
+| 
+| Note : You must put all files into the folder /assets/
+| 
+| Example of use :
+|
+| $assets['css'] = array(
+| 
+| 	array(
+| 
+| 		'name' => 'layout',
+| 		'version' => 0.1,
+| 		'src' => 'css/layout.css',
+| 		'compress' => TRUE
+| 
+| 		),
+| 
+| 	array(
+| 
+| 		'name' => 'bootstrap',
+| 		'version' => 0.2,
+| 		'src' => 'libs/css/bootstrap/bootstrap.css'
+| 		'compress' => TRUE
+| 	)
+| 
+| 	);
+| 
+*/
+$assets['css'] = array();
+
+/*
+|--------------------------------------------------------------------------
+| Register js
+|--------------------------------------------------------------------------
+|
+| Here you can register all js used by your application
+| 
+| Note : You must put all files into the folder /assets/
+| 
+| Example of use :
+|
+| $assets['js'] = array(
+| 
+| 	array(
+| 
+| 		'name' => 'toast',
+| 		'version' => '',
+| 		'src' => 'libs/js/toast.js',
+| 		'compress' => TRUE
+| 
+| 		)
+| 	);
+| 
+*/
+$assets['js'] = array();
+
+
+/* End of file assets.php */
+/* Location: ./application/config/assets.php */
