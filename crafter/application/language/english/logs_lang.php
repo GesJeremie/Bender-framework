ba4c3ad505de3b60b61960fe70e583f852eabdef
Jeremie Ges <bonjour@gesjeremie.fr>
2014-01-25 17:38:35 +0100

    drop crafter

--- crafter/application/language/english/logs_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang['error'] = 'Error';
-$lang['debug'] = 'Debug';
-$lang['info'] = 'Info';
-
-/* End of file logs_lang.php */
-/* Location: ./application/language/english/logs_lang.php */
--- index.php
+++ index.php
@@ -19,6 +19,7 @@
  *
  */
 	define('ENVIRONMENT', 'development');
+	// test
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
